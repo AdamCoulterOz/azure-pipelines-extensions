e2012c91aea30ca340a7b9af16b3df03966eff04
Adam Coulter <ads@me.com>
2019-12-17 16:27:53 +1100

    Created Tests Helpers

---
+++ Extensions/Terraform/Src/Tasks/Terraform/TerraformV0/Tests/Helpers.ts
@@ -0,0 +1,88 @@
+interface IEndpointConfig {
+
+    name: string;
+    Init();
+}
+
+class EndpointConfigAWS implements IEndpointConfig {
+
+    name: string;
+    username: string;
+    password: string;
+    region: string;
+
+    constructor(username: string, password: string, region: string) {
+        this.name = "aws";
+        this.username = username;
+        this.password = password;
+        this.region = region;
+    }
+
+    public Init() {
+        process.env['ENDPOINT_AUTH_SCHEME_AWS'] = 'Basic';
+        process.env['ENDPOINT_AUTH_PARAMETER_AWS_USERNAME'] = this.username;
+        process.env['ENDPOINT_AUTH_PARAMETER_AWS_PASSWORD'] = this.password;
+        process.env['ENDPOINT_AUTH_PARAMETER_AWS_REGION'] = this.region;
+    }
+}
+
+class EndpointConfigAzure implements IEndpointConfig {
+
+    name: string;
+    subscriptionId: string;
+    spId: string;
+    spKey: string;
+    tenantId: string;
+
+    constructor(subscriptionId: string, spId: string, spKey: string, tenantId: string) {
+        this.name = "azure";
+        this.subscriptionId = subscriptionId;
+        this.spId = spId;
+        this.spKey = spKey;
+        this.tenantId = tenantId;
+    }
+
+    public Init() {
+        process.env['ENDPOINT_AUTH_SCHEME_AzureRM'] = 'ServicePrincipal';
+        process.env['ENDPOINT_DATA_AzureRM_SUBSCRIPTIONID'] = this.subscriptionId;
+        process.env['ENDPOINT_AUTH_PARAMETER_AzureRM_SERVICEPRINCIPALID'] = this.spId;
+        process.env['ENDPOINT_AUTH_PARAMETER_AzureRM_SERVICEPRINCIPALKEY'] = this.spKey;
+        process.env['ENDPOINT_AUTH_PARAMETER_AzureRM_TENANTID'] = this.tenantId;
+    }
+}
+
+class EndpointConfigGoogle implements IEndpointConfig {
+
+    name: string;
+    project: string;
+    issuer: string;
+    audience: string;
+    privateKey: string;
+    scope: string;
+
+    constructor(project: string, issuer: string, audience: string, privateKey: string, scope: string) {
+        this.name = "google";
+        this.project = project;
+        this.issuer = issuer;
+        this.audience = audience;
+        this.privateKey = privateKey;
+        this.scope = scope;
+    }
+
+    public Init() {
+        process.env['ENDPOINT_AUTH_SCHEME_GCP'] = 'Jwt';
+        process.env['ENDPOINT_DATA_GCP_PROJECT'] = this.project;
+        process.env['ENDPOINT_AUTH_PARAMETER_GCP_ISSUER'] = this.issuer;
+        process.env['ENDPOINT_AUTH_PARAMETER_GCP_AUDIENCE'] = this.audience;
+        process.env['ENDPOINT_AUTH_PARAMETER_GCP_PRIVATEKEY'] = this.privateKey;
+        process.env['ENDPOINT_AUTH_PARAMETER_GCP_SCOPE'] = this.scope;
+    }
+}
+
+function initProviders() {
+    let providers = [];
+    providers.push(new EndpointConfigAWS("DummyUsername", "DummyPassword", "DummyRegion"));
+    providers.push(new EndpointConfigAzure("DummySubscriptionId", "DummyServicePrincipalId", "DummyServicePrincipalKey", "DummyTenantId"));
+    providers.push(new EndpointConfigGoogle("DummyProject", "DummyIssuer", "DummyAudience", "DummyPrivateKey", "DummyScope"));
+    return providers;
+}
